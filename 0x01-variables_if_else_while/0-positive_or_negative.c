585027ab8f2d14ff47f51c1873cf878d5fa25242
FaresAhmed74 <faresahmed987@gmail.com>
2023-07-14 17:46:39 -0700

    s

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,19 +2,25 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/* betty style doc for function main goes there *
+ */
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
-	   printf("%d is positive\n",n);
-	else if (n==0)
-	    printf("%d is zero\n",n);
+	if (n > 0)
+	   printf("%d is positive\n", n);
+	else if (n == 0)
+	    printf("%d is zero\n", n);
 	else
-	    printf("%d is negative\n",n);
+	    printf("%d is negative\n", n);
 	return (0);
 }
 
